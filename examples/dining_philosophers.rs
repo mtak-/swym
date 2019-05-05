99fd95b0879ec4fed6f6e4f5bd989e9f774602a7
tyler <tyler@brainiumstudios.com>
2019-05-05 15:28:09 -0700

    count how many retries are requested in dining_philosopher's example

--- examples/dining_philosophers.rs
+++ examples/dining_philosophers.rs
@@ -1,4 +1,5 @@
 use crossbeam_utils::thread;
+use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
 use swym::{
     tcell::TCell,
     thread_key,
@@ -23,6 +24,9 @@ impl Fork {
 
 /// a bit contrived
 fn main() {
+    let total_retry_count = AtomicUsize::new(0);
+    let total_retry_count = &total_retry_count;
+
     let mut forks = Vec::new();
     for _ in 0..NUM_PHILOSOPHERS {
         forks.push(Fork::new());
@@ -33,12 +37,14 @@ fn main() {
             let left_fork = &forks[i];
             let right_fork = &forks[(i + 1) % NUM_PHILOSOPHERS];
             scope.spawn(move |_| {
+                let mut retry_count = 0;
                 let thread_key = thread_key::get();
                 for i in 0..FOOD_ITERATIONS {
                     thread_key.rw(|tx| {
                         if left_fork.in_use.get(tx, Ordering::default())?
                             || right_fork.in_use.get(tx, Ordering::default())?
                         {
+                            retry_count += 1;
                             Err(Error::RETRY)
                         } else {
                             left_fork.in_use.set(tx, true)?;
@@ -56,8 +62,10 @@ fn main() {
                         Ok(())
                     })
                 }
+                total_retry_count.fetch_add(retry_count, Relaxed);
             });
         }
     })
     .unwrap();
+    println!("Total Retry Count: {:?}", total_retry_count.load(Relaxed));
 }
