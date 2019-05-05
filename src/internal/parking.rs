7555b52549a9b3f83141d58684c4aa163cafc453
tyler <tyler@brainiumstudios.com>
2019-05-05 15:29:13 -0700

    panic in debug on invalid RETRY, otherwise just park the thread forever

--- src/internal/parking.rs
+++ src/internal/parking.rs
@@ -14,29 +14,27 @@ fn key() -> usize {
 #[inline(never)]
 #[cold]
 pub fn park<'tx, 'tcell>(pin: &PinMutRef<'tx, 'tcell>) {
-    if pin.parkable() {
-        // TODO: stats
-        unsafe {
-            let key = key();
-            let park_token = ParkToken(pin.park_token());
-            let validate = move || pin.park_validate();
-            let before_sleep = || {};
-            let timed_out = |_, _| {};
-            match parking_lot_core::park(key, validate, before_sleep, timed_out, park_token, None) {
-                ParkResult::Unparked(token) => debug_assert_eq!(token, DEFAULT_UNPARK_TOKEN),
-                ParkResult::Invalid => {}
-                ParkResult::TimedOut => {
-                    if cfg!(debug_assertions) {
-                        panic!("unexpected timeout on parked thread")
-                    }
+    debug_assert!(
+        pin.parkable(),
+        "`RETRY` on a transaction that has an empty read set causes the thread to sleep forever \
+         in release"
+    );
+    // TODO: stats
+    unsafe {
+        let key = key();
+        let park_token = ParkToken(pin.park_token());
+        let validate = move || pin.park_validate();
+        let before_sleep = || {};
+        let timed_out = |_, _| {};
+        match parking_lot_core::park(key, validate, before_sleep, timed_out, park_token, None) {
+            ParkResult::Unparked(token) => debug_assert_eq!(token, DEFAULT_UNPARK_TOKEN),
+            ParkResult::Invalid => {}
+            ParkResult::TimedOut => {
+                if cfg!(debug_assertions) {
+                    panic!("unexpected timeout on parked thread")
                 }
             }
         }
-    } else {
-        panic!(
-            "requesting a RETRY on a transaction with an empty read and write set puts the thread \
-             to sleep forever"
-        );
     }
 }
 
