87a44b48fb6355e847aac7255d0b288c475d0197
tyler <tyler@brainiumstudios.com>
2019-05-10 10:59:06 -0700

    failing to unpark test

---
+++ tests/unpark.rs
@@ -0,0 +1,77 @@
+mod unpark {
+    use crossbeam_utils::thread;
+    use swym::{tcell::TCell, thread_key, tx::Status};
+
+    // This test attempts to create a situation where a thread can fail to park on `a` & `b`.
+    // Specifically, parking validation fails on `b`. This causes the thread to try to clear the
+    // unpark bit it might have just set on `a`. This is incorrect if there is another thread parked
+    // on `a`.
+    #[test]
+    fn park_failure() {
+        const ITER: usize = 1_000;
+        const TRIES: usize = 100;
+
+        // if we haven't completed in a reasonable amount of time, abort, failing the test
+        std::thread::spawn(|| {
+            std::thread::sleep(std::time::Duration::from_secs(10));
+            std::process::abort();
+        });
+
+        for _ in 0..TRIES {
+            let a = TCell::new(0);
+            let b = TCell::new(0);
+
+            thread::scope(|s| {
+                // this thread updates b
+                s.spawn(|_| {
+                    let key = thread_key::get();
+                    for x in 0..=ITER {
+                        key.rw(|tx| {
+                            b.set(tx, x)?;
+                            Ok(())
+                        });
+                    }
+                });
+                // this thread attempts to ensure a == b
+                s.spawn(|_| {
+                    let key = thread_key::get();
+                    loop {
+                        let finished = key.rw(|tx| {
+                            let snap_a = a.get(tx, Default::default())?;
+                            let snap_b = b.get(tx, Default::default())?;
+                            if snap_a != snap_b {
+                                a.set(tx, snap_b)?;
+                            }
+                            Ok(snap_a == ITER)
+                        });
+                        if finished {
+                            break;
+                        }
+                    }
+                });
+                // competing parked threads
+                for _ in 0..16 {
+                    // this thread waits for a == b
+                    s.spawn(|_| {
+                        let key = thread_key::get();
+                        loop {
+                            let finished = key.rw(|tx| {
+                                let snap_a = a.get(tx, Default::default())?;
+                                let snap_b = b.get(tx, Default::default())?;
+                                if snap_a != snap_b {
+                                    Err(Status::AWAIT_RETRY)
+                                } else {
+                                    Ok(snap_a == ITER)
+                                }
+                            });
+                            if finished {
+                                break;
+                            }
+                        }
+                    });
+                }
+            })
+            .unwrap()
+        }
+    }
+}
