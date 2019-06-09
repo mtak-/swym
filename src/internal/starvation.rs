b36b889a275615a58a75896fa08fc4d224e11c24
tyler <tyler@brainiumstudios.com>
2019-06-09 13:37:02 -0700

    pretty decent starvation handling. finally improves some benchmarks

--- src/internal/starvation.rs
+++ src/internal/starvation.rs
@@ -17,13 +17,13 @@ use crate::{
 };
 use core::{
     cell::Cell,
-    num::NonZeroU32,
+    num::{NonZeroU32, NonZeroUsize},
     sync::atomic::{self, AtomicUsize, Ordering::Relaxed},
 };
 use parking_lot_core::{self, FilterOp, ParkResult, ParkToken, UnparkResult, UnparkToken};
 use std::thread;
 
-const NO_STARVERS_EPOCH: usize = 0;
+const NO_STARVERS: usize = 0;
 const SPIN_LIMIT: u32 = 6;
 const YIELD_LIMIT: u32 = 10;
 
@@ -34,80 +34,85 @@ const YIELD_LIMIT: u32 = 10;
 const MAX_ELAPSED_EPOCHS: usize = 64 * TICK_SIZE;
 
 static STARVATION: Starvation = Starvation {
-    blocked_epoch: AtomicUsize::new(NO_STARVERS_EPOCH),
+    starved_token: AtomicUsize::new(NO_STARVERS),
 };
 
 /// `Starvation` only uses `Relaxed` memory ` ordering.
 struct Starvation {
-    blocked_epoch: AtomicUsize,
+    starved_token: AtomicUsize,
 }
 
 impl Starvation {
     #[inline]
-    fn starve_lock(&self, epoch: QuiesceEpoch) {
+    fn starve_lock(&self, token: NonZeroUsize) {
         if self
-            .blocked_epoch
-            .compare_exchange_weak(NO_STARVERS_EPOCH, epoch.get().get(), Relaxed, Relaxed)
+            .starved_token
+            .compare_exchange_weak(NO_STARVERS, token.get(), Relaxed, Relaxed)
             .is_err()
         {
-            drop(self.starve_lock_slow(epoch));
+            drop(self.starve_lock_slow(token));
         }
     }
 
     #[inline]
-    fn starve_unlock<F: FnOnce()>(&self, non_inlined_work: F) {
+    fn starve_unlock<F: FnOnce(), G: FnMut(NonZeroUsize) -> bool, U: FnOnce(NonZeroUsize)>(
+        &self,
+        non_inlined_work: F,
+        should_upgrade: G,
+        upgrade: U,
+    ) {
         // If a thread is starving, unparking is usually gonna happen.
         //
         // There's also not much to gain from a fast path as starvation handling is already in a
         // deeply slow path.
-        self.starve_unlock_slow(non_inlined_work);
+        self.starve_unlock_slow(non_inlined_work, should_upgrade, upgrade);
     }
 
     #[inline]
-    fn wait_for_starvers<F: FnMut() -> Option<QuiesceEpoch>>(&self, should_upgrade: F) {
-        let blocked_epoch = self.blocked_epoch.load(Relaxed);
-        if unlikely!(blocked_epoch != NO_STARVERS_EPOCH) {
-            self.wait_for_starvers_slow(should_upgrade)
+    fn wait_for_starvers(&self, token: NonZeroUsize) {
+        let starved_token = self.starved_token.load(Relaxed);
+        if unlikely!(starved_token != NO_STARVERS) {
+            self.wait_for_starvers_slow(token)
         }
     }
 
     #[cold]
     #[inline(never)]
-    fn starve_lock_slow(&self, epoch: QuiesceEpoch) {
-        let mut blocked_epoch = self.blocked_epoch.load(Relaxed);
+    fn starve_lock_slow(&self, token: NonZeroUsize) {
+        let mut starved_token = self.starved_token.load(Relaxed);
         loop {
-            if blocked_epoch == NO_STARVERS_EPOCH {
-                match self.blocked_epoch.compare_exchange_weak(
-                    NO_STARVERS_EPOCH,
-                    epoch.get().get(),
+            if starved_token == NO_STARVERS {
+                match self.starved_token.compare_exchange_weak(
+                    NO_STARVERS,
+                    token.get(),
                     Relaxed,
                     Relaxed,
                 ) {
                     Ok(_) => return,
-                    Err(x) => blocked_epoch = x,
+                    Err(x) => starved_token = x,
                 }
                 continue;
             }
 
             // Park our thread until we are woken up by an unlock
             let addr = self as *const _ as usize;
-            let validate = || self.blocked_epoch.load(Relaxed) != NO_STARVERS_EPOCH;
+            let validate = || self.starved_token.load(Relaxed) != NO_STARVERS;
             let before_sleep = || {};
             let timed_out = |_, _| {};
-            let park_token = ParkToken(epoch.get().get());
+            let park_token = ParkToken(token.get());
             match unsafe {
                 parking_lot_core::park(addr, validate, before_sleep, timed_out, park_token, None)
             } {
-                ParkResult::Unparked(UnparkToken(wakeup_epoch)) => {
+                ParkResult::Unparked(UnparkToken(wakeup_token)) => {
                     debug_assert!(
-                        wakeup_epoch != NO_STARVERS_EPOCH,
+                        wakeup_token != NO_STARVERS,
                         "unfairly unparking a starving thread"
                     );
-                    if wakeup_epoch == epoch.get().get() {
+                    if wakeup_token == token.get() {
                         debug_assert_eq!(
-                            wakeup_epoch,
-                            self.blocked_epoch.load(Relaxed),
-                            "improperly set the blocked_epoch before handing off starvation \
+                            wakeup_token,
+                            self.starved_token.load(Relaxed),
+                            "improperly set the starved_token before handing off starvation \
                              control"
                         );
                         return;
@@ -116,83 +121,95 @@ impl Starvation {
                 ParkResult::Invalid => {}
                 ParkResult::TimedOut => debug_assert!(false),
             }
-            blocked_epoch = self.blocked_epoch.load(Relaxed);
+            starved_token = self.starved_token.load(Relaxed);
         }
     }
 
     #[cold]
     #[inline(never)]
-    fn wait_for_starvers_slow<F: FnMut() -> Option<QuiesceEpoch>>(&self, mut should_upgrade: F) {
-        let mut blocked_epoch = self.blocked_epoch.load(Relaxed);
+    fn wait_for_starvers_slow(&self, token: NonZeroUsize) {
+        let mut starved_token = self.starved_token.load(Relaxed);
         loop {
-            if blocked_epoch == NO_STARVERS_EPOCH {
+            if starved_token == NO_STARVERS {
                 return;
             }
 
             // Park our thread until we are woken up by an unlock
             let addr = self as *const _ as usize;
-            let validate = || self.blocked_epoch.load(Relaxed) != NO_STARVERS_EPOCH;
+            let validate = || self.starved_token.load(Relaxed) != NO_STARVERS;
             let before_sleep = || {};
             let timed_out = |_, _| {};
-            if let Some(epoch) = should_upgrade() {
-                return self.starve_lock_slow(epoch);
-            }
             match unsafe {
                 parking_lot_core::park(
                     addr,
                     validate,
                     before_sleep,
                     timed_out,
-                    ParkToken(NO_STARVERS_EPOCH),
+                    ParkToken(token.get()),
                     None,
                 )
             } {
-                ParkResult::Unparked(UnparkToken(NO_STARVERS_EPOCH)) => {
+                ParkResult::Unparked(UnparkToken(NO_STARVERS)) => {
                     return;
                 }
-                ParkResult::Unparked(_) => {}
+                ParkResult::Unparked(UnparkToken(wakeup_token)) => {
+                    if wakeup_token == token.get() {
+                        // this thread has been upgraded to a starver
+                        debug_assert_eq!(
+                            wakeup_token,
+                            self.starved_token.load(Relaxed),
+                            "improperly set the starved_token before handing off starvation \
+                             control"
+                        );
+                    }
+                    return;
+                }
                 ParkResult::Invalid => {}
                 ParkResult::TimedOut => debug_assert!(false),
             }
-            blocked_epoch = self.blocked_epoch.load(Relaxed);
+            starved_token = self.starved_token.load(Relaxed);
         }
     }
 
     #[cold]
     #[inline(never)]
-    fn starve_unlock_slow<F: FnOnce()>(&self, non_inlined_work: F) {
+    fn starve_unlock_slow<F: FnOnce(), G: FnMut(NonZeroUsize) -> bool, U: FnOnce(NonZeroUsize)>(
+        &self,
+        non_inlined_work: F,
+        mut should_upgrade: G,
+        upgrade: U,
+    ) {
         non_inlined_work();
 
         let addr = self as *const _ as usize;
-        let starve_epoch = Cell::new(NO_STARVERS_EPOCH);
-        let starve_epoch = &starve_epoch;
+        let next_starved_token = Cell::new(NO_STARVERS);
+        let next_starved_token = &next_starved_token;
 
         // We don't know what thread we wish to unpark until we finish filtering. This means that
         // threads will sometimes be unparked without the possibility of making progress.
-        let filter = |token: ParkToken| {
-            let epoch = starve_epoch.get();
-            if epoch == NO_STARVERS_EPOCH {
-                starve_epoch.set(token.0);
-                FilterOp::Unpark
-            } else if token.0 == NO_STARVERS_EPOCH {
-                FilterOp::Skip
-            } else if token.0 < epoch {
-                starve_epoch.set(token.0);
+        let filter = move |token: ParkToken| {
+            debug_assert!(token.0 != NO_STARVERS, "invalid ParkToken detected");
+            let next_starved = next_starved_token.get();
+            if next_starved == NO_STARVERS {
+                if should_upgrade(unsafe { NonZeroUsize::new_unchecked(token.0) }) {
+                    next_starved_token.set(token.0);
+                }
                 FilterOp::Unpark
             } else {
                 FilterOp::Skip
             }
         };
         let callback = |unpark_result: UnparkResult| {
-            debug_assert_ne!(self.blocked_epoch.load(Relaxed), NO_STARVERS_EPOCH);
-            let epoch = starve_epoch.get();
-            self.blocked_epoch.store(epoch, Relaxed);
-            debug_assert!(epoch == NO_STARVERS_EPOCH || unpark_result.unparked_threads > 0);
-            UnparkToken(epoch)
+            debug_assert_ne!(self.starved_token.load(Relaxed), NO_STARVERS);
+            let next_starved = next_starved_token.get();
+            self.starved_token.store(next_starved, Relaxed);
+            drop(NonZeroUsize::new(next_starved).map(|this| upgrade(this)));
+            debug_assert!(next_starved == NO_STARVERS || unpark_result.unparked_threads > 0);
+            UnparkToken(next_starved)
         };
 
         let result = unsafe { parking_lot_core::unpark_filter(addr, filter, callback) };
-        if starve_epoch.get() != NO_STARVERS_EPOCH {
+        if next_starved_token.get() != NO_STARVERS {
             stats::starvation_handoff();
         }
         stats::blocked_by_starvation(result.unparked_threads)
@@ -208,6 +225,34 @@ enum ProgressImpl {
     Starving,
 }
 
+impl ProgressImpl {
+    #[inline]
+    fn new() -> Self {
+        ProgressImpl::NotStarving {
+            first_failed_epoch: None,
+            backoff:            unsafe { NonZeroU32::new_unchecked(1) },
+        }
+    }
+
+    #[inline]
+    fn should_starve(&self) -> bool {
+        match self {
+            ProgressImpl::NotStarving {
+                first_failed_epoch: Some(_),
+                backoff,
+            } => backoff.get() >= YIELD_LIMIT,
+            ProgressImpl::NotStarving {
+                first_failed_epoch: None,
+                ..
+            } => false,
+            ProgressImpl::Starving => {
+                debug_assert!(false);
+                false
+            }
+        }
+    }
+}
+
 pub struct Progress {
     inner: Cell<ProgressImpl>,
 }
@@ -219,7 +264,7 @@ impl Drop for Progress {
             ProgressImpl::NotStarving {
                 first_failed_epoch: None,
                 backoff,
-            } if backoff.get() == 0 => {}
+            } if backoff.get() == 1 => {}
             _ => panic!("`Progress` dropped without having made progress"),
         }
     }
@@ -229,10 +274,7 @@ impl Progress {
     #[inline]
     pub fn new() -> Self {
         Progress {
-            inner: Cell::new(ProgressImpl::NotStarving {
-                first_failed_epoch: None,
-                backoff:            NonZeroU32::new(1).unwrap(),
-            }),
+            inner: Cell::new(ProgressImpl::new()),
         }
     }
 
@@ -249,6 +291,8 @@ impl Progress {
                 if backoff.get() <= SPIN_LIMIT {
                     let now = EPOCH_CLOCK.now().unwrap_or_else(|| abort!());
                     if now.get().get() - epoch.get().get() >= MAX_ELAPSED_EPOCHS {
+                        // long transaction detected, `spin_loop_hint` is probably a bad backoff
+                        // strategy.
                         self.inner.set(ProgressImpl::NotStarving {
                             first_failed_epoch: Some(epoch),
                             backoff:            unsafe {
@@ -262,17 +306,15 @@ impl Progress {
                             atomic::spin_loop_hint();
                         }
                     }
-                } else {
+                } else if backoff.get() <= YIELD_LIMIT {
                     thread::yield_now();
-                }
 
-                if backoff.get() <= YIELD_LIMIT {
                     self.inner.set(ProgressImpl::NotStarving {
                         first_failed_epoch: Some(epoch),
                         backoff:            unsafe { NonZeroU32::new_unchecked(backoff.get() + 1) },
                     });
                 } else {
-                    STARVATION.starve_lock(epoch);
+                    STARVATION.starve_lock(self.to_token());
                     self.inner.set(ProgressImpl::Starving)
                 }
             }
@@ -285,9 +327,7 @@ impl Progress {
     #[inline]
     pub fn wait_for_starvers(&self) {
         match self.inner.get() {
-            ProgressImpl::NotStarving { .. } => {
-                STARVATION.wait_for_starvers(|| self.should_upgrade())
-            }
+            ProgressImpl::NotStarving { .. } => STARVATION.wait_for_starvers(self.to_token()),
             ProgressImpl::Starving => {}
         };
     }
@@ -298,56 +338,31 @@ impl Progress {
         match self.inner.get() {
             ProgressImpl::NotStarving { .. } => {}
             ProgressImpl::Starving => {
-                STARVATION.starve_unlock(|| {
-                    self.inner.set(ProgressImpl::NotStarving {
-                        first_failed_epoch: None,
-                        backoff:            NonZeroU32::new(1).unwrap(),
-                    })
-                });
+                STARVATION.starve_unlock(
+                    || self.inner.set(ProgressImpl::new()),
+                    |this| {
+                        unsafe { Self::from_token(this) }
+                            .inner
+                            .get()
+                            .should_starve()
+                    },
+                    |this| {
+                        unsafe { Self::from_token(this) }
+                            .inner
+                            .set(ProgressImpl::Starving)
+                    },
+                );
             }
         };
     }
 
     #[inline]
-    fn should_upgrade(&self) -> Option<QuiesceEpoch> {
-        None
-        // match self.inner.get() {
-        //     ProgressImpl::NotStarving {
-        //         first_failed_epoch: Some(first_failed_epoch),
-        //         backoff,
-        //     } => {
-        //         if backoff.get() <= SPIN_LIMIT {
-        //             let now = EPOCH_CLOCK.now().unwrap_or_else(|| abort!());
-        //             if now.get().get() - first_failed_epoch.get().get() >= MAX_ELAPSED_EPOCHS {
-        //                 self.inner.set(ProgressImpl::NotStarving {
-        //                     first_failed_epoch: Some(first_failed_epoch),
-        //                     backoff:            unsafe {
-        //                         NonZeroU32::new_unchecked(SPIN_LIMIT + 1)
-        //                     },
-        //                 });
-        //                 thread::yield_now();
-        //                 return None;
-        //             } else {
-        //                 for _ in 0..1 << backoff.get() {
-        //                     atomic::spin_loop_hint();
-        //                 }
-        //             }
-        //         } else {
-        //             thread::yield_now();
-        //         }
+    fn to_token(&self) -> NonZeroUsize {
+        unsafe { NonZeroUsize::new_unchecked(self as *const Self as usize) }
+    }
 
-        //         if backoff.get() <= YIELD_LIMIT {
-        //             self.inner.set(ProgressImpl::NotStarving {
-        //                 first_failed_epoch: Some(first_failed_epoch),
-        //                 backoff:            unsafe { NonZeroU32::new_unchecked(backoff.get() + 1)
-        // },             });
-        //         } else {
-        //             epoch = Some(first_failed_epoch);
-        //             self.inner.set(ProgressImpl::Starving);
-        //         }
-        //     }
-        //     _ => {}
-        // };
-        // epoch
+    #[inline]
+    unsafe fn from_token(this: NonZeroUsize) -> &'static Self {
+        &*(this.get() as *const Self)
     }
 }
