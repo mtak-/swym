8df1039c418cf2dd5e6552f7d589afa671e507f7
tyler <tyler@brainiumstudios.com>
2019-06-23 15:32:07 -0700

    workaround for criterion asan incompatibility

--- ci/rbtree.sh
+++ ci/rbtree.sh
@@ -30,43 +30,11 @@ cargo check --features debug-alloc,nightly,stats,$RTM --benches --bins --example
 # run tests
 ./x.py test
 RUST_TEST_THREADS=1 cargo test --features stats,nightly,$RTM --lib --tests
-RUST_TEST_THREADS=1 RUSTFLAGS="${RUSTFLAGS} ${ASAN_FLAG}" \
-    time cargo test \
-        --features debug-alloc,stats,$RTM
+
+# TODO: address sanitizer doesn't work with criterion?
+# RUSTFLAGS="${RUSTFLAGS} ${ASAN_FLAG}"
+RUST_TEST_THREADS=1 \
+    time cargo test --features debug-alloc,stats,$RTM
 
 # benchmarks
-./x.py bench insert:: --features nightly,$RTM 
-
-# these benchmarks are run one at a time due to high memory usage
-./x.py bench --features nightly,$RTM rbtree::contains_key_01
-./x.py bench --features nightly,$RTM rbtree::contains_key_02
-./x.py bench --features nightly,$RTM rbtree::contains_key_03
-./x.py bench --features nightly,$RTM rbtree::contains_key_04
-./x.py bench --features nightly,$RTM rbtree::contains_key_05
-./x.py bench --features nightly,$RTM rbtree::contains_key_06
-./x.py bench --features nightly,$RTM rbtree::contains_key_07
-./x.py bench --features nightly,$RTM rbtree::contains_key_08
-./x.py bench --features nightly,$RTM rbtree::entry_01
-./x.py bench --features nightly,$RTM rbtree::entry_02
-./x.py bench --features nightly,$RTM rbtree::entry_03
-./x.py bench --features nightly,$RTM rbtree::entry_04
-./x.py bench --features nightly,$RTM rbtree::entry_05
-./x.py bench --features nightly,$RTM rbtree::entry_06
-./x.py bench --features nightly,$RTM rbtree::entry_07
-./x.py bench --features nightly,$RTM rbtree::entry_08
-./x.py bench --features nightly,$RTM rbtree::get_01
-./x.py bench --features nightly,$RTM rbtree::get_02
-./x.py bench --features nightly,$RTM rbtree::get_03
-./x.py bench --features nightly,$RTM rbtree::get_04
-./x.py bench --features nightly,$RTM rbtree::get_05
-./x.py bench --features nightly,$RTM rbtree::get_06
-./x.py bench --features nightly,$RTM rbtree::get_07
-./x.py bench --features nightly,$RTM rbtree::get_08
-./x.py bench --features nightly,$RTM rbtree::insert_01
-./x.py bench --features nightly,$RTM rbtree::insert_02
-./x.py bench --features nightly,$RTM rbtree::insert_03
-./x.py bench --features nightly,$RTM rbtree::insert_04
-./x.py bench --features nightly,$RTM rbtree::insert_05
-./x.py bench --features nightly,$RTM rbtree::insert_06
-./x.py bench --features nightly,$RTM rbtree::insert_07
-./x.py bench --features nightly,$RTM rbtree::insert_08
+./x.py bench --features nightly,$RTM
